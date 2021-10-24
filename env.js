c61db0c2c5bf79fc3996cdb12ace1ab1c16aa4fb
Eban <account+github@eban.bzh>
2021-10-24 18:54:36 +0200

    Create env.js

---
+++ env.js
@@ -0,0 +1,19 @@
+// Notion Todo
+const todoNotionToken = "" // Here goes your notion app token
+const todoDbId = "" // Here goes your notion todo DB ID
+
+// openWeather
+const openWeatherApiKey = "" // Here goes your openWeather API key
+const myCity = "" // Here goes your city
+
+// Spotify
+const spotifyRefreshToken = "" // Here goes your spotify refresh token, to generate one please see https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
+const spotifyClientID = "" // Here goes your spotify app client ID
+const spotifyClientSecret = "" // Here goes your spotify app client secret
+
+// Tasmota
+const tasmotaIP = "" 
+
+// GoAccess
+const goAccessDataURL = "" // Here goes your goaccess json data URL
+const goAccessAuth = "" // Here goes the creds for goaccess json serving web server auth
